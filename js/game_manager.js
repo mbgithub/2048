d2d5ef4c63aea6f3ce8375cda52374598db8660e
mbgithub <madhavanbalaji@hotmail.com>
2014-10-09 20:02:34 -0500

    Change to play up to 8192
    Take Udacity version of 2048 game and make it play up to 8192.  Make the
    grid 5X5 square instead of 4X4.  Also make the starting tile to be a 4
    or an 8 and get rid the the tile for number 2.

--- js/game_manager.js
+++ js/game_manager.js
@@ -77,7 +77,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 4 : 8;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -176,7 +176,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          if (merged.value === 8192) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
